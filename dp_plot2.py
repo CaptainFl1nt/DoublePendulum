45c40a1f8f82e381359f7c5db78a7d1e39e98172
CaptainFl1nt <70005775+CaptainFl1nt@users.noreply.github.com>
2021-01-05 20:29:57 -0800

    Add files via upload

---
+++ dp_plot2.py
@@ -0,0 +1,45 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+L = [0,0]
+m = [0,0]
+ext = "o5"
+with open("params_"+ext+".csv", "r") as f:
+    params = f.readline().strip().split(",")
+    L[0] = float(params[0])
+    L[1] = float(params[1])
+    m[0] = float(params[2])
+    m[1] = float(params[3])
+
+time, phi1, phi2 = np.loadtxt("output_"+ext+".csv",delimiter=",",usecols=(0,1,2),unpack=True)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.xaxis.set_visible(False)
+ax.yaxis.set_visible(False)
+
+def animate(t):
+    ax.clear()
+    ax.set_xlim(-1.1*(L[0]+L[1]),1.1*(L[0]+L[1]))
+    ax.set_ylim(-1.1*(L[0]+L[1]),1.1*(L[0]+L[1]))
+    ax.set_aspect('equal')
+    if t < 100:
+        s = 0
+    else:
+        s = t - 100
+    ax.plot(L[0]*np.sin(phi1[s:t])+L[1]*np.sin(phi2[s:t]),-L[0]*np.cos(phi1[s:t])-L[1]*np.cos(phi2[s:t]),color="lightgreen")
+
+    xpoints = [0,L[0]*math.sin(phi1[t]),L[0]*math.sin(phi1[t])+L[1]*math.sin(phi2[t])]
+    ypoints = [0,-L[0]*math.cos(phi1[t]),-L[0]*math.cos(phi1[t])-L[1]*math.cos(phi2[t])]
+    ax.plot(xpoints,ypoints,color="brown")
+
+    ax.scatter([0],[0])
+    circle1 = plt.Circle((xpoints[1],ypoints[1]),0.08,color='brown')
+    circle2 = plt.Circle((xpoints[2],ypoints[2]),0.08,color='brown')
+    ax.add_artist(circle1)
+    ax.add_artist(circle2)
+
+anim = animation.FuncAnimation(fig,animate,frames=len(time),interval=1,blit=False)
+plt.show()
