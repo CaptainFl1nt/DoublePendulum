c17075b6965916a47b7926e153c661fa19a32878
CaptainFl1nt <70005775+CaptainFl1nt@users.noreply.github.com>
2021-01-16 20:57:10 -0800

    Update RungeKuttaSolver.hpp

--- RungeKuttaSolver.hpp
+++ RungeKuttaSolver.hpp
@@ -10,14 +10,10 @@ class RungeKuttaSolver : public ODESolver {
         RungeKuttaSolver() : ODESolver() { }
         RungeKuttaSolver(vector<double> (*func)(vector<double>, double), vector<double> y0) : ODESolver(func,y0) { }
         RungeKuttaSolver(DifferentialEquation* diffEqn, vector<double> y0) : ODESolver(diffEqn,y0) { }
-        void Solve();
+        vector<double> next(vector<double> y, double t);
 };
 
-void RungeKuttaSolver::Solve() {
-    solution = vector<vector<double>>((int) (timetotal/dt)+1);
-    
-    double t;
-    vector<double> y(dim);
+vector<double> RungeKuttaSolver::next(vector<double> y, double t) {
     vector<double> dy(dim);
     vector<double> k1(dim);
     vector<double> k2(dim);
@@ -26,32 +22,32 @@ void RungeKuttaSolver::Solve() {
     vector<double> ytemp1(dim);
     vector<double> ytemp2(dim);
     vector<double> ytemp3(dim);
-    
-    y = yinitial;
-    for (int j = 0; j*dt <= timetotal; j++) {
-        solution[j] = y;
-        t = j*dt;
-        for(int i=0; i<dim; i++) {
-           k1[i] = dt*wrap(y,t)[i];
-           ytemp1[i] = y[i] + 0.5*k1[i];
-        }
-        for(int i=0; i<dim; i++) {
-          k2[i] = dt*wrap(ytemp1,t+dt/2.0)[i];
-          ytemp2[i] = y[i] + 0.5*k2[i];
-        }
-        for(int i=0; i<dim; i++) {
-          k3[i] = dt*wrap(ytemp2,t+dt/2.0)[i];
-          ytemp3[i] = y[i] + k3[i];
-        }
-        for(int i=0; i<dim; i++) {
-          k4[i] = dt*wrap(ytemp3,t+dt)[i];
-        }
-        for(int i=0; i<dim; i++) {
-          dy[i] = (k1[i]+2.0*k2[i]+2.0*k3[i]+k4[i])/6.0;
-        }
-        for(int i=0; i<dim; i++) {
-          y[i] += dy[i];
-        }
+
+    vector<double> fval = wrap(y,t);
+    for(int i=0; i<dim; i++) {
+        k1[i] = dt*fval[i];
+        ytemp1[i] = y[i] + 0.5*k1[i];
     }
+    fval = wrap(ytemp1,t+dt/2.0);
+    for(int i=0; i<dim; i++) {
+        k2[i] = dt*fval[i];
+        ytemp2[i] = y[i] + 0.5*k2[i];
+    }
+    fval = wrap(ytemp2,t+dt/2.0);
+    for(int i=0; i<dim; i++) {
+        k3[i] = dt*fval[i];
+        ytemp3[i] = y[i] + k3[i];
+    }
+    fval = wrap(ytemp3,t+dt);
+    for(int i=0; i<dim; i++) {
+        k4[i] = dt*fval[i];
+    }
+    for(int i=0; i<dim; i++) {
+        dy[i] = (k1[i]+2.0*k2[i]+2.0*k3[i]+k4[i])/6.0;
+    }
+    for(int i=0; i<dim; i++) {
+        y[i] += dy[i];
+    }
+    return y;
 }
 #endif
